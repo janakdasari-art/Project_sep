10c4e4b1453f788c7d190d6f96f9088faed55fac
janakdasari-art <janakdasari@gmail.com>
2026-09-19 01:06:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ terraform {
 }
 
 provider "aws" {
-  region = "eu-north-1"
+  region = "us-east-1"
 }
 
 resource "aws_security_group" "k8s_sg" {
@@ -56,7 +56,7 @@ resource "aws_security_group" "k8s_sg" {
 resource "aws_instance" "k8s_node" {
   ami           = "ami-0aba19e56f3eaec05"
   instance_type = "c7i-flex.large"
-  key_name      = "project"
+  key_name      = "new"
 
   vpc_security_group_ids = [
     aws_security_group.k8s_sg.id
